8187758037a7908dc32be65f3c9937c08c778b06
Muhammadarif98 <67822369+Muhammadarif98@users.noreply.github.com>
2024-08-15 00:18:39 +0300

    Task008Test solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,37 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var titleTextView: TextView
+    private lateinit var changeButton: Button
+    private var isTitleVisible = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        titleTextView = findViewById(R.id.titleTextView)
+        changeButton = findViewById(R.id.hideButton)
+
+        if (savedInstanceState != null) {
+            isTitleVisible = savedInstanceState.getBoolean("isTitleVisible", true)
+            titleTextView.visibility = if (isTitleVisible) View.VISIBLE else View.GONE
+        } else {
+            titleTextView.text = "Hello World!"
+        }
+
+        changeButton.setOnClickListener {
+            isTitleVisible = !isTitleVisible
+            titleTextView.visibility = if (isTitleVisible) View.VISIBLE else View.GONE
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("isTitleVisible", titleTextView.visibility == View.VISIBLE)
     }
 }
