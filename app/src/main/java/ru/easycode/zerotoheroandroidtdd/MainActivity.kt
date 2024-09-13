b0f4fa3516319dd232b30a987da0dd90c2144a9f
Muhammadarif98 <67822369+Muhammadarif98@users.noreply.github.com>
2024-09-13 11:34:22 +0300

    task 011 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,41 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
+    private lateinit var linearLayout: LinearLayout
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.countTextView)
+        linearLayout = findViewById(R.id.rootLayout)
+        button = findViewById(R.id.incrementButton)
+
+        button.setOnClickListener {
+            textView.text = Count.Base(step = 2).increment(number = textView.text.toString())
+        }
+
+        if (savedInstanceState == null) {
+            textView.text = "0"
+        } else {
+            textView.text = savedInstanceState.getString("count")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("count", textView.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        textView.text = savedInstanceState.getString("count")
     }
 }
