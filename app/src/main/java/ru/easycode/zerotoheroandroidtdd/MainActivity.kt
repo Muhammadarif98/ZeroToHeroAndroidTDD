9c5b02559eeccbf9a807f2922056f32e695a50b5
Muhammadarif98 <67822369+Muhammadarif98@users.noreply.github.com>
2024-09-13 10:07:45 +0300

    task 010 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,31 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
+    private lateinit var rootLayout: LinearLayout
+    private lateinit var removeButton: Button
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.titleTextView)
+        rootLayout = findViewById(R.id.rootLayout)
+        removeButton = findViewById(R.id.removeButton)
+
+        removeButton.setOnClickListener {
+            rootLayout.removeView(textView)
+            removeButton.isEnabled = false
+        }
+        if (savedInstanceState != null){
+            rootLayout.removeView(textView)
+            removeButton.isEnabled = false
+        }
     }
 }
